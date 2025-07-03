4c17b2ba0e4ab52b0472393e0bc4c7aa53ebb29a
prijall <khadkaprijal@gmail.com>
2025-07-03 20:52:28 +0545

    searching 2d matrix

---
+++ DSA/Search_2D_Matrix.py
@@ -0,0 +1,38 @@
+class solution:
+    def mat(self, matrix:list[list[int]], target:int)->bool:
+        rows, cols=len(matrix[1]), len(matrix[0])
+        top, down=0, rows-1
+
+        while top<=down:
+            row=top + (down-top)//2
+
+            if target>matrix[row][-1]:
+                top=row+1
+            elif target < matrix[row][0]:
+                down=row-1
+            else:
+                break
+
+        row= top + (down-top)//2
+
+        
+        l, r=0, cols-1
+
+        while l<=r:
+            m=l + (r-l)//2
+
+            if target> matrix[row][m]:
+                l=m+1
+            elif target < matrix[row][m]:
+                r=m-1
+            else:
+                return True
+            
+        if not (top<=down):
+            return False
+        
+        return False
+    
+
+s=solution()
+print(s.mat(matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 15))
